b91986fc8d7718f6125730dd95506bd833b4dab3
EnderWarik <Steam1117@mail.ru>
2022-02-25 20:14:31 +1000

    add valid token(Response header id user)

--- TOGUME/Controllers/AccountController.cs
+++ TOGUME/Controllers/AccountController.cs
@@ -1,11 +1,38 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.IdentityModel.Tokens.Jwt;
+using System.Net.Http.Headers;
 
 namespace TOGUME.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/v1/user/[controller]")]
     [ApiController]
     public class AccountController : ControllerBase
     {
+
+
+
+
+        [Route("")]
+        [HttpGet("")]
+        [Authorize]
+        public IActionResult Valid()
+        {
+            Console.WriteLine("To Valid");
+            // var stream = 
+            //Console.WriteLine(header);
+            //string jsonToken = header.Remove(0,7);
+            // Console.WriteLine(stream);
+            var jsonToken = new JwtSecurityTokenHandler().ReadToken(Request.Headers["Authorization"].ToString().Remove(0, 7));
+            //  var jsonToken = handler.ReadToken(stream);
+            var tokenS = jsonToken as JwtSecurityToken;
+            var jti = tokenS.Claims.First(claim => claim.Type == JwtRegisteredClaimNames.Sub).Value;
+           // Request.Headers.Add("Id", jti);
+            Response.Headers.Add("Id", jti);
+            //header.Add("Id", jti);
+            return Ok(new { id = jti });
+        }
+
     } //айдишник в хедеры при валид
 }
