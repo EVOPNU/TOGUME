8182131a9a78f6dbcc5979c4673a11946610bdef
EvoMGenius <apatrios@mail.ru>
2022-03-03 19:45:15 +1000

    init commit for T_01

---
+++ db-control-module/src/test/java/com/evo/apatrios/dbcontrolmodule/DbControlModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.evo.apatrios.dbcontrolmodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbControlModuleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
