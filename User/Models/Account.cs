0d380fc58272f88ade131f9643631cf1276ded14
EnderWarik <Steam1117@mail.ru>
2022-03-06 12:08:48 +1000

    T_02

---
+++ User/Models/Account.cs
@@ -0,0 +1,17 @@
+﻿namespace User
+{
+    public class Account
+    {
+
+        public int id { get; set; }
+       // public string? firstname { get; set; }
+       // public string? lastname { get; set; }
+       // public string? patronymic { get; set; }
+        public string? email{ get; set; }
+        public string? password { get; set; }
+
+   //датакреате датарожде
+
+    }
+
+}
