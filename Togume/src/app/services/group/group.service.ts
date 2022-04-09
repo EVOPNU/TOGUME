aa051b41401f044ffd0666589a947295f2f32b47
unknown <merkulovwork@mail.ru>
2022-04-09 17:26:39 +1000

    designed groupService.ts

--- Togume/src/app/services/group/group.service.ts
+++ Togume/src/app/services/group/group.service.ts
@@ -1,65 +1,69 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Group } from './group';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GroupService {
 
-  constructor() { }
+  myHeader = new HttpHeaders().set('access_token','Bearer '+localStorage.getItem('access_token'))
 
-  createNewGroup(){
+  constructor(public http:HttpClient) { }
 
+  createNewGroup(id:Group['id'],name:Group['name'],description:Group['description'],access:Group['access'],amount:number){
+    return this.http.post('/api/v1/Groups/create',{id,name,description,access,amount},{headers:this.myHeader})
   }
 
-  changeDataOfGroup(){
-
+  changeDataOfGroup(id:Group['id'],name:Group['name'],description:Group['description'],access:Group['access'],amount:number){
+    return this.http.post('/api/v1/Groups/update',{id,name,description,access,amount},{headers:this.myHeader})
   }
 
-  addNewUserInGroup(){
-
+  addNewUserInGroup(access:Group['access'],groupId:Group['id'],userId:Group['id']){
+    return this.http.get('/api/v1/Groups/add/'+access+'/'+groupId+'/'+userId,{headers:this.myHeader})
   }
   
-  showAllUsersGroups(){
-
+  showAllUsersGroups(userId:Group['id']){
+    return this.http.get('/api/v1/Groups/us/'+userId,{headers:this.myHeader})
   }
 
-  applicationForMembership(){
-
+  applicationForMembership(groupId:Group['id']){
+   return this.http.get('/api/v1/Groups/req/'+groupId,{headers:this.myHeader})
   }
 
-  showAllApplicationForMembershipOfGroup(){
-
+  showAllApplicationForMembershipOfGroup(groupId:Group['id']){
+   return this.http.get('/api/v1/Groups/invgr/'+groupId,{headers:this.myHeader})
   }
 
   showAllUsersApplicationForMembership(){
-
+   return this.http.get('/api/v1/Groups/invus',{headers:this.myHeader})
   }
 
-  deleteApplicationForMembership(){
-
+  deleteApplicationForMembership(groupId:Group['id'],userId:Group['id']){
+   return this.http.get('/api/v1/Groups/not/'+groupId+'/'+userId,{headers:this.myHeader})
   }
 
-  appointAsModerator(){
-
+  appointAsModerator(groupId:Group['id'],userId:Group['id']){
+   return this.http.get('/api/v1/Groups/right/'+groupId+'/'+userId,{headers:this.myHeader})
   }
 
-  deleteOrLeaveOfGroup(){
-
+  deleteOrLeaveOfGroup(groupId:Group['id'],userId:Group['id']){
+   return this.http.get('/api/v1/Groups/delete/'+groupId+'/'+userId,{headers:this.myHeader})
   }
 
-  destroyGroup(){
-
+  destroyGroup(groupId:Group['id']){
+   return this.http.get('/api/v1/Groups/exterminatus/'+groupId,{headers:this.myHeader})
   }
-  // ????
-  dataOfGroup(){
-
+  
+  dataOfGroup(groupId:Group['id']){
+   return this.http.get('/api/v1/Groups/gr/'+groupId,{headers:this.myHeader})
   }
 
-  showAllMembers(){
-
+  showAllMembers(groupId:Group['id']){
+   return this.http.get('/api/v1/Groups/ingroup/'+groupId,{headers:this.myHeader})
   }
 
   showAllGroups(){
-    
+   return this.http.get('/api/v1/Groups/allgroups',{headers:this.myHeader})
   }
 }
