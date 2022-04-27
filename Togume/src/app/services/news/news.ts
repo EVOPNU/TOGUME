95c3bc6340c5128f3c64321ec9a88625d68c3214
User-PC\User <merkulovwork@mail.ru>
2022-04-27 12:59:19 +1000

    designed NewsService

---
+++ Togume/src/app/services/news/news.ts
@@ -0,0 +1,9 @@
+export class News{
+    constructor(
+        public news_id:number,
+        public user_id:number,
+        public public_id:number,
+        public title:string,
+        public content:string
+    ){}
+}
